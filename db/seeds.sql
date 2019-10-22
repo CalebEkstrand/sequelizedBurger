e387c530c795cfe238d18568d9c299fdcfd4eb64
John Dee <ekstrandcaleb@gmail.com>
2019-10-22 15:11:06 -0600

    added and organized files.

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burger (name) VALUES ('Meeses');
+INSERT INTO burger (name) VALUES ('Bobbi');
+INSERT INTO burger (name, eaten) VALUES ('Sylvester', true);
+INSERT INTO burger (name, eaten) VALUES ('Marilyn Manson', true);
+INSERT INTO burger (name, eaten) VALUES ('Joe Biden', true);
+INSERT INTO burger (name) VALUES ('Owens');
+
